f1da5ba41294a9c8cb4b7268ea16665f28904fa3
Mohamed Diab <81900099+mokdiab@users.noreply.github.com>
2025-03-29 01:14:10 +0200

    addding images and banner on home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
-export default async function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import data from '@/lib/data'
+
+export default async function Page() {
+  return <HomeCarousel items={data.carousels} />
 }
