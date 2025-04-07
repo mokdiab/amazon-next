1e21603c82cc65701320905ae4e1268a74c32977
Mohamed Diab <81900099+mokdiab@users.noreply.github.com>
2025-04-07 23:50:04 +0200

    editing searchBar

--- components/shared/header/Search.tsx
+++ components/shared/header/Search.tsx
@@ -1,19 +1,24 @@
-import { SearchIcon, ChevronDownIcon } from "lucide-react"
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@radix-ui/react-select"
-import {APP_NAME } from "@/lib/constants"
-import { Input } from "../../ui/input"
-const categories = ['men', 'women', 'kids','accessories']
+import { SearchIcon } from 'lucide-react'
+import {
+  Select,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '@/components/ui/select'
+import { APP_NAME } from '@/lib/constants'
+import { Input } from '../../ui/input'
+const categories = ['men', 'women', 'kids', 'accessories']
 
 async function Search() {
   return (
-    <form action="/search" method="GET" className="flex items-stretch h-10">
-      <Select name="category" >
-        <SelectTrigger className="flex items-center px-3 gap-2 w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-r-none rounded-l-md">
-          <SelectValue placeholder="All" />
-          <ChevronDownIcon width={20} height={20}/>
+    <form action='/search' method='GET' className='flex  items-stretch h-10 '>
+      <Select name='category'>
+        <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r  rounded-r-none rounded-l-md rtl:rounded-r-md rtl:rounded-l-none'>
+          <SelectValue placeholder='All' />
         </SelectTrigger>
-        <SelectContent position="popper">
-          <SelectItem value="all">All</SelectItem>
+        <SelectContent position='popper'>
+          <SelectItem value='all'>All</SelectItem>
           {categories.map((category) => (
             <SelectItem key={category} value={category}>
               {category}
@@ -21,9 +26,18 @@ async function Search() {
           ))}
         </SelectContent>
       </Select>
-      <Input className="flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full" placeholder={`Search in ${APP_NAME}`} name="q" type="search" />
-      <button type="submit" className="bg-primary text-primary-foreground rounded-s-none rounded-e-md h-full px-3 py-2" aria-label="Search">
-        <SearchIcon className="w-6 h-6" />
+      <Input
+        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full'
+        placeholder={`Search in ${APP_NAME}`}
+        name='q'
+        type='search'
+      />
+      <button
+        type='submit'
+        className='bg-primary text-primary-foreground  rounded-s-none rounded-e-md h-full px-3 py-2 '
+        aria-label='Search'
+      >
+        <SearchIcon className='w-6 h-6' />
       </button>
     </form>
   )
